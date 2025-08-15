eb41bb1cc0f093d6e4e549cc5b895294a15c95bc
LcKnight <jzh1199+GitHub.LcKnight@gmail.com>
2025-08-15 21:37:10 +0800

    UE5 Time reversal power - #4 Limiting the power, implement the function to record the normal elapsed time, store it to limit the power to reverse time

--- Source/TimePower/TimeManager.cpp
+++ Source/TimePower/TimeManager.cpp
@@ -11,19 +11,59 @@ ATimeManager::ATimeManager()
 
 }
 
+void ATimeManager::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	//Store the time that has been reverse
+	if (TimeReverseEnable) {
+		CurrentRecordedTime = FMath::Min(CurrentRecordedTime + DeltaSeconds * CurrentTimeFactor, ReverseTimeMaximum);
+		if (CurrentRecordedTime <= 0.0f) {
+			EndTimeReverse();
+			CurrentRecordedTime = 0.0;
+		}
+	}
+}
+
 float ATimeManager::GetTimeFactor()
 {
 	return CurrentTimeFactor;
 }
 
+bool ATimeManager::GetTimeReverseAbility()
+{
+	return TimeReverseEnable && CurrentRecordedTime >= ReverseTimeThreshold;
+}
+
+float ATimeManager::GetCurrentReversableTime()
+{
+	return CurrentRecordedTime;
+}
+
 void ATimeManager::BeginTimeReverse()
 {
-	CurrentTimeFactor = ReverseTimeFactor;
+	if (GetTimeReverseAbility()) {
+		CurrentTimeFactor = ReverseTimeFactor;
+	}
 }
 
 void ATimeManager::EndTimeReverse()
 {
 	CurrentTimeFactor = NormalTimeFactor;
+}
+
+void ATimeManager::EnableTimeReverse()
+{
+	if (!TimeReverseEnable)
+		TimeReverseEnable = true;
+}
+void ATimeManager::DisableTimeReverse()
+{
+	if (TimeReverseEnable){
+		EndTimeReverse();
+		TimeReverseEnable = false;
+		CurrentRecordedTime = 0.f;
+
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/TimePower/TimeManager.h
+++ Source/TimePower/TimeManager.h
@@ -16,8 +16,17 @@ public:
 	// Sets default values for this actor's properties
 	ATimeManager();
 
+	void Tick(float DeltaSeconds) override;
+
+public:
 	UFUNCTION(BlueprintCallable)
 	float GetTimeFactor();
+
+	UFUNCTION(BlueprintCallable)
+	bool GetTimeReverseAbility();
+
+	UFUNCTION(BlueprintCallable)
+	float GetCurrentReversableTime();
 
 	//Time Reverse control
 	UFUNCTION(BlueprintCallable)
@@ -26,6 +35,11 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void EndTimeReverse();
 
+	UFUNCTION(BlueprintCallable)
+	void EnableTimeReverse();
+	UFUNCTION(BlueprintCallable)
+	void DisableTimeReverse();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -33,15 +47,32 @@ protected:
 	// THis is the current active factor of time
 	//UPROPERTY(BlueprintReadOnly)
 protected:
-	// This is the current active factor of time
+	// This is the current states
 	UPROPERTY(BlueprintReadOnly)
 	float CurrentTimeFactor = 1.0f;
 
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentRecordedTime = 0.0f;
+
+	//TIme reverse ability
+	UPROPERTY(BlueprintReadOnly)
+	bool TimeReverseEnable = false;
+
+
+
+
 public:
-	//magic time values
+	//global presets
 	UPROPERTY(BlueprintReadWrite,EditAnywhere)
 	float NormalTimeFactor = 1.0f;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	float ReverseTimeFactor = -3.0f;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float ReverseTimeThreshold = 3.0f;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float ReverseTimeMaximum = 15.0f;
+
 
 };
